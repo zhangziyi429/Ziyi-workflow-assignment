4dc4348bad6cf577bb9ecccf6fa09f8e8073c4b2
zhangziyi429 <zhangziyi429@gmail.com>
2021-03-15 18:07:50 -0700

    framework

--- Compiled data.R
+++ Compiled data.R
@@ -1,1 +1,23 @@
 
+install.packages("here")
+install.packages("vroom")
+
+library(tidyverse)
+library(here)
+library(vroom)
+library(readxl)
+
+rm(list = ls())
+
+# create a for loop for reading dataset
+
+
+# files <- list.files(here("Ziyi-workflow-assignment"), pattern = "Data entry.xlsx", recursive = T)
+# evaluation <- vroom(here("Ziyi-workflow-assignment",files))
+
+
+Data_entry_1 <- read_excel("Data entry_Becca.xlsx")
+Data_entry_2 <- read_excel("Data entry_Devon.xlsx")
+Data_entry_3 <- read_excel("Data entry_Karlee.xlsx")
+
+
